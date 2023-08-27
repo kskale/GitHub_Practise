fc4c3d581ea9afa84fff5c2fc5c0876e9567af6a
kajos <kajol.shinde@capgemini.com>
2023-08-27 17:17:17 +0530

    shifted in project folder

--- Practise_1.java
+++
@@ -1,5 +0,0 @@
-public class Practise_1 {
-    public void first_class(){
-        system.out.println("hello")
-    }
-}
---
+++ project/Practise_1.java
@@ -0,0 +1,8 @@
+public class Practise_1 {
+    public static void first_class(){
+        System.out.println("hello Practise_1");
+    }
+    public static void main(String[] args){
+        first_class();
+    }
+}
